6c1358dffc064c2ac9ce6b394fbf829361ade89e
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2020-04-14 13:18:04 -0400

    Update robotics.py

--- robotics.py
+++ robotics.py
@@ -20,8 +20,8 @@ class User():
         self.student_num = student_num
 
 class Log():
-    def __init__ (self, ttime: str, user_id: int, part_id: int, quantity: int):
-        self.ttime = ttime
+    def __init__ (self, time: str, user_id: int, part_id: int, quantity: int):
+        self.time = time
         self.user_id = user_id
         self.part_id = part_id
         self.quantity = quantity
