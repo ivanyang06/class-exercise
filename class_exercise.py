7a37187de18e4d9d35cfd3e8f37d0007d869d81a
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2020-04-14 12:51:28 -0400

    Update class_exercise.py

--- class_exercise.py
+++ class_exercise.py
@@ -1,1 +1,20 @@
+class Person():
+    def __init__ (self, name, height, strength, health_points):
+        self.name = name
+        self.height = height
+        self.strength = strength
+        self.health_points = health_points
+    
+    def __str__ (self):
+        return f"{self.name} {self.health_points}"
+    
+    def introduce(self):
+        return f"Hello, my name is {self.name}"
 
+    def punch(self, person):
+        person.health_points -= 10
+george = Person("george", 69, 0, 10)
+david = Person("david", 99999, -1, 11)
+
+george.punch(david)
+print(david.health_points)
